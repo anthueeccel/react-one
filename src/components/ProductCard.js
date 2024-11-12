786711770151e10fd2be17f0592bde32183249ea
anthue.pereira <anthue.pereira@intl.shijigroup.com>
2024-11-12 16:50:03 +0000

    new project - product

---
+++ src/components/ProductCard.js
@@ -0,0 +1,36 @@
+export function ProductCard({
+    product,
+    background = "slategray",
+    ...restProps
+}) {
+
+    function getProductPrice(price) {
+        return price;
+    }
+
+    return (
+        <article style={{
+            background,
+            width: "100%",
+            border: '1px solid white',
+            borderRadius: "8px",
+            padding: "16px",
+            textAlign: "center"
+        }}>
+
+            <h2>{product.title}</h2>
+            <img
+                src={product.imageSrc}
+                alt={product.title}
+                {...restProps}
+            />
+            <p>Specification:</p>
+            <ul style={{ listStyle: 'none', padding: 'none' }}>
+                <li>{product.specification[0]}</li>
+                <li>{product.specification[1]}</li>
+                <li>{product.specification[2]}</li>
+            </ul>
+            <button>Buy (From ${getProductPrice(product.price)})</button>
+        </article>
+    );
+}
